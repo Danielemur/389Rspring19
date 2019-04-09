830bc18c5cb1e19ed8028bd76b1efb7d83c7ac11
Daniel Kelly <dk97@comcast.net>
2019-04-09 17:12:18 -0400

    HW 6 WIP.

---
+++ assignments/6_Forensics_II/fpff.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+import sys
+import struct
+
+# Constants
+MAGIC = 0x8BADF00D
+VERSION = 1
+
+WORD_SIZE = 4
+DWORD_SIZE = 8
+DOUBLE_SIZE = 8
+
+# Exit on failure conditions
+def fail(msg):
+    sys.exit(msg)
+
+def remove_padding(s):
+    try:
+        null_idx = s.index('\0')
+    except ValueError:
+        return s
+
+    num_null = len(s) - null_idx
+    if s.endswith(num_null * '\0'):
+        return s[0:null_idx]
+    else:
+        raise ValueError
+
+class FPFF_Section:
+
+    def _parse_ascii(data, length):
+        ascii_bytes = data[0:length]
+
+        try:
+            return ascii_bytes.decode('ascii')
+        except UnicodeDecodeError:
+            fail("Bad section value! Expected ASCII-encoded text")
+
+    def _parse_utf8(data, length):
+        utf8_bytes = data[0:length]
+
+        try:
+            return utf8_bytes.decode('utf-8')
+        except UnicodeDecodeError:
+            fail("Bad section value! Expected UTF-8-encoded text")
+
+    def _parse_words(data, length):
+        try:
+            return [elmt[0] for elmt in struct.iter_unpack('<L', data[0:length])]
+        except struct.error:
+            fail("Bad section value! Length must be divisible by 4 for WORDS type")
+
+    def _parse_dwords(data, length):
+        try:
+            return [elmt[0] for elmt in struct.iter_unpack('<Q', data[0:length])]
+        except struct.error:
+            fail("Bad section value! Length must be divisible by 8 for DWORDS type")
+
+    def _parse_doubles(data, length):
+        try:
+            return [elmt[0] for elmt in struct.iter_unpack('<d', data[0:length])]
+        except struct.error:
+            fail("Bad section value! Length must be divisible by 8 for DOUBLES type")
+
+    def _parse_coord(data, length):
+        try:
+            return struct.unpack('<dd', data[0:length])
+        except struct.error:
+            fail("Bad section value! Length must be 16 for COORD type")
+
+    def _parse_reference(data, length):
+        try:
+            struct.unpack('<L', data[0:length])
+        except struct.error:
+            fail("Bad section value! Length must be 4 for REFERENCE type")
+
+    def _parse_png(data, length):
+        return b'\211PNG\r\n\032\n' + data[0:length]
+
+    def _parse_gif87(data, length):
+        return b'GIF87a' + data[0:length]
+
+    def _parse_gif89(data, length):
+        return b'GIF89a' + data[0:length]
+
+    SECTION_ASCII = 0x1     # ASCII-encoded text (https://en.wikipedia.org/wiki/ASCII)
+    SECTION_UTF8 = 0x2      # UTF-8-encoded text (https://en.wikipedia.org/wiki/UTF-8)
+    SECTION_WORDS = 0x3     # Array of words.
+    SECTION_DWORDS = 0x4    # Array of dwords.
+    SECTION_DOUBLES = 0x5   # Array of doubles.
+    SECTION_COORD = 0x6     # (Latitude, longitude) tuple of doubles.
+    SECTION_REFERENCE = 0x7 # The index of another section.
+    SECTION_PNG = 0x8       # Embedded PNG image.
+    SECTION_GIF87 = 0x9     # Embedded GIF87.
+    SECTION_GIF89 = 0xA     # Embedded GIF89.
+
+    PARSE_BY_TYPE = {
+        SECTION_ASCII: _parse_ascii,
+        SECTION_UTF8: _parse_utf8,
+        SECTION_WORDS: _parse_words,
+        SECTION_DWORDS: _parse_dwords,
+        SECTION_DOUBLES: _parse_doubles,
+        SECTION_COORD: _parse_coord,
+        SECTION_REFERENCE: _parse_reference,
+        SECTION_PNG: _parse_png,
+        SECTION_GIF87: _parse_gif87,
+        SECTION_GIF89: _parse_gif89
+    }
+
+    def __init__(self, data, num_sections):
+
+        # Unpack section data
+        (self.stype,
+         self.slen) = struct.unpack_from("<LL", data)
+
+        # Validate and parse type
+        offset = 2 * WORD_SIZE
+        try:
+            self.svalue = self.PARSE_BY_TYPE[self.stype](data[offset:], self.slen)
+        except KeyError:
+            fail("Bad section type! Got %s, "
+                 "expected value between 0x1 and 0xA, inclusive" % (hex(magic)))
+
+        if self.stype == self.SECTION_REFERENCE and self.svalue >= num_sections:
+            fail("Bad section value! Value for REFERENCE must be in the range [0, nsects - 1]")
+
+    def size(self):
+        return self.slen + 2 * WORD_SIZE
+
+    def type_str(self):
+        name = {
+            self.SECTION_ASCII: 'SECTION_ASCII',
+            self.SECTION_UTF8: 'SECTION_UTF8',
+            self.SECTION_WORDS: 'SECTION_WORDS',
+            self.SECTION_DWORDS: 'SECTION_DWORDS',
+            self.SECTION_DOUBLES: 'SECTION_DOUBLES',
+            self.SECTION_COORD: 'SECTION_COORD',
+            self.SECTION_REFERENCE: 'SECTION_REFERENCE',
+            self.SECTION_PNG: 'SECTION_PNG',
+            self.SECTION_GIF87: 'SECTION_GIF87',
+            self.SECTION_GIF89: 'SECTION_GIF89'
+        }
+        return name[self.stype]
+
+class FPFF:
+
+    HEADER_SIZE = 4 * WORD_SIZE + 8
+
+    def __init__(self, data):
+
+        # Unpack header data
+        (self.magic,
+         self.version,
+         self.timestamp,
+         author_bytes,
+         self.section_count) = struct.unpack_from("<LLl8sL", data)
+
+        # Validate header
+        if self.magic != MAGIC:
+            fail("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
+
+        if self.version != VERSION:
+            fail("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
+
+        try:
+            self.author = remove_padding(author_bytes.decode('ascii'))
+        except UnicodeDecodeError:
+            fail("Bad author! Expected ASCII-encoded string")
+        except ValueError:
+            fail("Bad author! String is not properly padded")
+
+        if self.section_count == 0:
+            fail("Bad section count! Got %d, expected value >0" % (int(self.section_count)))
+
+        self.sections = self.section_count * [None]
+
+        offset = self.HEADER_SIZE
+        for i in range(0, self.section_count):
+            self.sections[i] = FPFF_Section(data[offset:], self.section_count)
+            offset += self.sections[i].size()
+
+# if len(sys.argv) < 2:
+#     sys.exit("Usage: python3 fpff.py input_file.fpff")
+
+# # Open file to parse
+# with open(sys.argv[1], 'rb') as file:
+#     data = file.read()
+
+# fpff = FPFF(data)
+
+# print("------- HEADER -------")
+# print("MAGIC: %s" % hex(fpff.header.magic))
+# print("VERSION: %d" % int(fpff.header.version))
+# print("TIMESTAMP: %d" % int(version))
+# print("AUTHOR: %s" % fpff.header.author)
+# print("SECTION COUNT: %d" % int(fpff.header.section_count))
+
+# print("-------  BODY  -------")
+# #for section in fpff.sections:
+
--- assignments/6_Forensics_II/writeup/README.md
+++ assignments/6_Forensics_II/writeup/README.md
@@ -13,11 +13,13 @@ Digital acknowledgement: Daniel Kelly
 
 #### 1.
 
-The IP address that has been attacked is 142.93.136.81.
-
+The IP address that has been attacked is 142.93.136.81, which is easily identified as it is the only one involved in every entry in the log.
 
 
 #### 2.
+
+By examining the logs, we see one particular IP address attempting to connect to many different ports (indicated by the `SYN` messsage sent).
+The `RST, ACK` response indicates that the port is not open, while a `SYN, ACK` indicates that it is.
 
 The log shows the attackers querying a myriad of commonly used ports, which suggests that they are using a port scanning tool.
 This is similar to the behavior of nmap used with the fast option.
@@ -28,7 +30,8 @@ After this is complete, the remainder of the attack seems to be carried out manu
 #### 3.
 
 The hackers' IP address is 159.203.113.181.
-They appear to be located in Clifton, New Jersey.
+This is evident as it is both the most prevalent IP interacting with the server by far, but also because it is the IP involved in the suspicious behavor described above.
+By entering this IP in an online geolocation lookup at `iplocation.net`, I found that they appear to be located in Clifton, New Jersey.
 
 
 
@@ -40,15 +43,15 @@ The attackers are using port 21, the File Transfer Protocol control port, to ste
 
 #### 5.
   
-The attackers stole the file `find_me.jpeg`, an image file.
-This photo was taken in Maldonado Department, Uraguay, on December 23, 2018 at 5:16:24 PM.
-It was taken with an iPhone 8 at an altitude of 4.5726 meters above sea level.
+The attackers stole the file `find_me.jpeg`, an image file, which is evident by looking at the log entry with info `Request: RETR find_me.jpeg`, as well as the subsequent transmissions using the `FTP-DATA` protocol.
+Using the command line tool exif, we find lat-long coordinates and a timestamp which indicate that this photo was taken in Maldonado Department, Uraguay, on December 23, 2018 at 5:16:24 PM.
+The command also shows that was taken with an iPhone 8 at an altitude of 4.5726 meters above sea level.
 
 
 
 #### 6.
 
-The attackers left a file called `greetz.fpff` on the server.
+The attackers left a file called `greetz.fpff` on the server, evidenced by the entry with info `Request: STOR greetz.fpff`
 
 
 
@@ -63,4 +66,36 @@ Alternatively, they could implement an IP whitelist or blacklist, either only pe
 
 ### Part 2 (55 Pts)
 
-*Replace this text with your repsonse to our prompt for part 2!*
+#### 1.
+
+#### 2.
+
+##### 1.
+
+##### 2.
+
+The file `greetz.fpff` was authored by `fl1nch`.
+
+##### 3.
+
+* Section 1:
+  * Type: SECTION_ASCII
+  * Data: 
+* Section 2:
+  * Type: SECTION_COORD
+  * Data: (52.336035, 4.880673)
+* Section 3:
+  * Type: SECTION_PNG
+  * Data: 'Hey you, keep looking :)'
+* Section 4:
+  * Type: SECTION_ASCII
+  * Data: '}R983CSMC_perg_tndid_u0y_yllufep0h{-R983CSMC'
+* Section 5:
+  * Type: SECTION_ASCII
+  * Data: 'Q01TQzM4OVIte2hleV9oM3lfeTBVX3lvdV9JX2RvbnRfbGlrZV95b3VyX2Jhc2U2NF9lbmNvZGluZ30='
+
+##### 4.
+
+##### 5.
+
+
